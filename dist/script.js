8e70981002d06f65dd556e30d7a0d352fa2e5111
unknown <maun2velsket@gmail.com>
2023-07-03 12:32:52 +0200

    Merge only main

--- dist/script.js
+++ dist/script.js
@@ -4,18 +4,12 @@ var htmlClassScore = document.querySelector(".score");
 var htmlClassGuess = document.querySelector(".guess");
 var htmlClasscheck = document.querySelector(".check");
 var htmlClassHighScore = document.querySelector(".highscore");
+var htmlClassAgain = document.querySelector(".again");
 htmlClassMessage.textContent = "❌Wrong or 🟢Right guess Number!";
-htmlClassNumber.textContent = "?";
 htmlClassScore.textContent = "10";
 htmlClassHighScore.textContent = "0";
 var highScoreCount = 0;
-// let highScoreCount:number=(htmlClassHighScore.textContent as unknown) as number;
-var againFn = function () {
-    htmlClassMessage.textContent = "❌Wrong or 🟢Right, guess Number!";
-    htmlClassNumber.textContent = "?";
-    htmlClassScore.textContent = "10";
-    htmlClassHighScore.textContent = "0";
-};
+var stateOfPlay = true;
 var getRandomNum = function () {
     //creates random guess number for game;
     var randomNum = Math.floor(Math.random() * 20) + 1;
@@ -24,27 +18,52 @@ var getRandomNum = function () {
     console.log("====================================");
     return randomNum;
 };
+var guessCheckNumber = getRandomNum();
+// let highScoreCount:number=(htmlClassHighScore.textContent as unknown) as number;
+var againFn = function () {
+    htmlClassMessage.textContent = "❌Wrong or 🟢Right, guess Number!";
+    guessCheckNumber = getRandomNum();
+    htmlClassNumber.textContent = String(guessCheckNumber);
+    htmlClassScore.textContent = "10";
+    document.querySelector("body").style.backgroundColor = "#222";
+    htmlClassNumber.style.width = "14rem";
+    console.log('====================================');
+    console.log(stateOfPlay);
+    console.log('====================================');
+    return stateOfPlay = true;
+};
+htmlClassNumber.textContent = String(guessCheckNumber);
 var checkGuessNumber = function (guess) {
-    if (guess === +htmlClassNumber.textContent) {
+    if (guess === guessCheckNumber) {
         console.log("====================================");
-        console.log("worked");
+        console.log(guess);
         console.log("====================================");
         highScoreCount++;
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        htmlClassNumber.style.width = "25rem";
         htmlClassMessage.textContent = "🎉 Correct Number!";
+        htmlClassNumber.textContent = String(guessCheckNumber);
         htmlClassHighScore.textContent = String(highScoreCount);
-        htmlClassNumber.textContent = String(getRandomNum());
+        stateOfPlay = false;
         return;
     }
     htmlClassMessage.textContent = "💥 Wrong Number!";
     htmlClassScore.textContent = String(htmlClassScore.textContent - 1);
 };
+//butttons
+//===============check guess btn==========================
 htmlClasscheck.addEventListener("click", function () {
-    if (+htmlClassScore.textContent === 0)
-        return alert("All chances used up please 'Try again' :-)");
-    if (!htmlClassGuess.value)
-        return alert("Please insert a Guess number between 1 and 20.");
-    var guessNumber = +htmlClassGuess.value;
-    if (guessNumber && guessNumber >= 1 && guessNumber <= 20) {
-        checkGuessNumber(guessNumber);
+    if (stateOfPlay) {
+        if (+htmlClassScore.textContent === 0)
+            return alert("All chances used up please 'Try again' :-)");
+        if (!htmlClassGuess.value)
+            return alert("Please insert a Guess number between 1 and 20.");
+        var guessNumber = +htmlClassGuess.value;
+        if (guessNumber && guessNumber >= 1 && guessNumber <= 20) {
+            checkGuessNumber(guessNumber);
+        }
     }
+    ;
 });
+//=====================click agin btn=====================
+htmlClassAgain.addEventListener("click", againFn);
--- src/script.ts
+++ src/script.ts
@@ -1,44 +1,56 @@
-const htmlClassMessage = document.querySelector(
-  ".message"
-) as HTMLParagraphElement;
+const htmlClassMessage = document.querySelector(".message") as HTMLParagraphElement;
 const htmlClassNumber = document.querySelector(".number") as HTMLDivElement;
 const htmlClassScore = document.querySelector(".score") as HTMLSpanElement;
 const htmlClassGuess = document.querySelector(".guess") as HTMLInputElement;
 const htmlClasscheck = document.querySelector(".check") as HTMLButtonElement;
-const htmlClassHighScore = document.querySelector(
-  ".highscore"
-) as HTMLSpanElement;
+const htmlClassHighScore = document.querySelector(".highscore") as HTMLSpanElement;
+const htmlClassAgain=document.querySelector(".again") as HTMLButtonElement;
 htmlClassMessage.textContent = "❌Wrong or 🟢Right guess Number!";
-htmlClassNumber.textContent = "?";
 htmlClassScore.textContent = "10";
 htmlClassHighScore.textContent = "0";
 
 let highScoreCount: number = 0;
-// let highScoreCount:number=(htmlClassHighScore.textContent as unknown) as number;
-const againFn=():void=>{
-    htmlClassMessage.textContent = "❌Wrong or 🟢Right, guess Number!";
-    htmlClassNumber.textContent = "?";
-    htmlClassScore.textContent = "10";
-    htmlClassHighScore.textContent = "0";
-};
-const getRandomNum = (): string => {
+let stateOfPlay=true;
+const getRandomNum = (): number => {
     //creates random guess number for game;
-  const randomNum = Math.floor(Math.random() * 20) + 1;
+  const randomNum:number = Math.floor(Math.random() * 20) + 1;
   console.log("====================================");
   console.log(randomNum);
   console.log("====================================");
-  return (randomNum as unknown) as string;
+  return randomNum;
+};
+let guessCheckNumber:number=getRandomNum();
+
+// let highScoreCount:number=(htmlClassHighScore.textContent as unknown) as number;
+const againFn=():boolean=>{
+    htmlClassMessage.textContent = "❌Wrong or 🟢Right, guess Number!";
+    guessCheckNumber=getRandomNum();
+    htmlClassNumber.textContent = String(guessCheckNumber);
+    htmlClassScore.textContent = "10";
+    document.querySelector("body").style.backgroundColor="#222";
+    htmlClassNumber.style.width="14rem";
+    console.log('====================================');
+    console.log(stateOfPlay);
+    console.log('====================================');
+   return stateOfPlay=true;
+
 };
 
+
+htmlClassNumber.textContent = String(guessCheckNumber);
+
 const checkGuessNumber = function (guess: number): void {
-  if (guess === (+(htmlClassNumber.textContent as unknown) as number)) {
-    console.log("====================================");
-    console.log("worked");
-    console.log("====================================");
+  if (guess === guessCheckNumber) {
+      console.log("====================================");
+      console.log(guess);
+      console.log("====================================");
     highScoreCount++;
+     document.querySelector("body").style.backgroundColor="#60b347";
+     htmlClassNumber.style.width="25rem";
     htmlClassMessage.textContent = "🎉 Correct Number!";
+    htmlClassNumber.textContent = String(guessCheckNumber);
     htmlClassHighScore.textContent = String(highScoreCount);
-    htmlClassNumber.textContent = String(getRandomNum());
+    stateOfPlay=false;
     return;
   }
   htmlClassMessage.textContent = "💥 Wrong Number!";
@@ -46,15 +58,22 @@ const checkGuessNumber = function (guess: number): void {
     (htmlClassScore.textContent as unknown as number) - 1
   );
 };
-
+//butttons
+//===============check guess btn==========================
 htmlClasscheck.addEventListener("click", () => {
-  if ((+(htmlClassScore.textContent as unknown) as number) === 0)
-    return alert("All chances used up please 'Try again' :-)");
-  if (!htmlClassGuess.value)
+    if(stateOfPlay){
+
+        if ((+(htmlClassScore.textContent as unknown) as number) === 0)
+        return alert("All chances used up please 'Try again' :-)");
+        if (!htmlClassGuess.value)
     return alert("Please insert a Guess number between 1 and 20.");
   const guessNumber: number = +(htmlClassGuess.value as unknown) as number;
 
   if (guessNumber && guessNumber >= 1 && guessNumber <= 20) {
     checkGuessNumber(guessNumber);
-  }
+}
+};
 });
+
+//=====================click agin btn=====================
+    htmlClassAgain.addEventListener("click",againFn);
